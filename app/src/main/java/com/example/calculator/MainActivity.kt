a2d9ea73d3cd8703e028cb76a73e4c273b6424e1
Czarek Karczewski <keracz1997@gmail.com>
2022-10-09 18:55:08 +0200

    add landscape layout and menu window

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.menu)
+
+        var button_simp : Button = findViewById<Button>(R.id.simple_calc)
+
+        button_simp.setOnClickListener(){
+            setContentView(R.layout.simple_calc)
+        }
+
+
     }
 }
